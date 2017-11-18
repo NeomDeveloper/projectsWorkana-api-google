83cfa43a25f0a9793ff2a1143c23acad5b96d6b7
Nizam Omari <nizam_omari>
2017-11-18 14:09:44 -0200

    '.'

--- index.php
+++ index.php
@@ -44,11 +44,6 @@ function getClient()
             // Exchange authorization code for an access token.
             $accessToken = $client->fetchAccessTokenWithAuthCode($_GET['code']);
 
-            // Store the credentials to disk.
-//        if (!file_exists(dirname($credentialsPath))) {
-//            mkdir(dirname($credentialsPath), 0700, true);
-//        }
-
             var_dump(file_put_contents($credentialsPath, json_encode($accessToken)));
             printf("Credentials saved to %s\n", $credentialsPath);
         } else {
@@ -89,7 +84,7 @@ function expandHomeDirectory($path)
  * can be used to indicate the authenticated user.
  * @return array Array of Messages.
  */
-function listMessages($service, $userId)
+function listMessages($service, $userId, $labelIds = array())
 {
     $pageToken = NULL;
     $messages = array();
@@ -99,6 +94,10 @@ function listMessages($service, $userId)
             if ($pageToken) {
                 $opt_param['pageToken'] = $pageToken;
             }
+            if (count($labelIds) > 0) {
+                $opt_param['labelIds'] = $labelIds;
+            }
+
             $messagesResponse = $service->users_messages->listUsersMessages($userId, $opt_param);
             if ($messagesResponse->getMessages()) {
                 $messages = array_merge($messages, $messagesResponse->getMessages());
@@ -111,64 +110,97 @@ function listMessages($service, $userId)
 
     foreach ($messages as $listUsersMessage) {
         $message = $service->users_messages->get('me', $listUsersMessage->getId());
-//        var_dump($message->);
-        echo 'ID message:  ' . $message->getId() . '<br><b>' . $message->getPayload()->getHeaders()[18]->value . '</b><br>';
-        echo $message->getSnippet();
-        echo '<br><br>';
-        exit;
+        $message_raw = $service->users_messages->get('me', $listUsersMessage->getId(), ['format' => 'raw']);
+        $subject = '(Sem assunto)';
+        $from = '';
+        echo '<pre>';var_dump($message->getPayload()->getParts());echo '</pre>';
+        foreach ($message->getPayload()->getParts() as $part) {
+            echo '<pre>';
+            var_dump($part->getbody());
+            echo '</pre>';
+        }
+
+        foreach ($message->getPayload()->getHeaders() as $header) {
+            if ($header->getName() == 'Subject') {
+                $subject = $header->getValue();
+            }
+            if ($header->getName() == 'From') {
+                $from = $header->getValue();
+            }
+        }
+
+        echo 'ID message: ' . $message->getId() . '<br>De: ' . $from . '<br>Assunto: <b>' . $subject . '</b><br>';
+        echo 'Snippet message: <b>' . $message->getSnippet() . '</b><br>';
+        echo 'ID thread: <b>' . $message_raw->getThreadId() . '</b><br>';
+        echo 'ID thread: <b>' . $message->getThreadId() . '</b><br>';
+        echo 'Mensagem original: <b>' . rtrim(strtr(base64_decode($message_raw->getRaw()), '+/', '-_'), '=') . '</b><br>';
+        echo '<br><a href="?delete_message=' . $message->getId() . '">Excluir mensagem: ' . $message->getId() . '</a>';
+        echo '<br><br><br><br>';
     }
 
     return $messages;
 }
 
+function delete_message($service, $userId, $messageId)
+{
+    $service->users_messages->trash($userId, $messageId);
+}
+
 try {
+
     $client = getClient();
     $_SESSION['client'] = $client;
 
     $service = new Google_Service_Gmail($client);
 
+    if (isset($_GET['delete_message']) && strlen($_GET['delete_message']) > 0) {
+        delete_message($service, 'me', $_GET['delete_message']);
+        $_GET['listar_emails'] = true;
+    }
+    echo 'Olá, ' . $service->users->getProfile('me')->getEmailAddress() . '<br><br>';
+    echo '<a href="?enviar_email=true">Clique aqui para enviar um email de teste</a> || ';
+    echo '<a href="?listar_emails=true">Clique aqui para listar todos os emails</a><br><br>';
+
     $user = 'me';
     $results = $service->users_labels->listUsersLabels($user);
-    echo '<b>Labels</b><br>';
+    echo '<br><b>Labels</b><br>';
     if (count($results->getLabels()) == 0) {
         print "No labels found.\n";
     } else {
         print "Labels:\n";
         foreach ($results->getLabels() as $label) {
-            print($label->getName() . " - ");
+            print('<a href="?list_emails_label=' . $label->getId() . '">' . $label->getName() . "</a> - ");
         }
     }
-    echo '<br>';
-//    foreach ($service->users_messages->listUsersMessages($user) as $listUsersMessage) {
-//        $message = $service->users_messages->get($user, $listUsersMessage->getId());
-//        echo '<b>' . $message->getPayload()->getHeaders()[18]->value . '</b><br>';
-//        echo $message->getSnippet();
-////        echo '<pre>';var_dump($message->getPayload());echo '</pre>';
-////        exit;
-//    }
-    if($_GET['enviar_email']) {
-        $strSubject = 'Test mail using GMail API' . date('M d, Y h:i:s A');
-        $strRawMessage = "From: Nizam <nizamomari1234@gmail.com>\r\n";
-        $strRawMessage .= "To: Nizam <nizam_omari@hotmail.com>\r\n";
-        $strRawMessage .= 'Subject: =?utf-8?B?' . base64_encode($strSubject) . "?=\r\n";
-        $strRawMessage .= "MIME-Version: 1.0\r\n";
-        $strRawMessage .= "Content-Type: text/html; charset=utf-8\r\n";
-        $strRawMessage .= 'Content-Transfer-Encoding: quoted-printable' . "\r\n\r\n";
-        $strRawMessage .= "this <b>is a test message!\r\n";
+    echo '<br><br>';
+    if (isset($_GET['enviar_email']) && $_GET['enviar_email']) {
+        echo '<form method="post">Assunto: <input name="subject"><br>Email: <input name="email_address"><br>Mensagem: <br><textarea name="message" id="" cols="30" rows="10"></textarea><button type="submit">Enviar</button></form>';
+        if (isset($_POST['email_address']) && strlen($_POST['email_address']) > 0 && isset($_POST['message']) && strlen($_POST['message']) > 0) {
+            $strSubject = $_POST['subject'];
+            $strRawMessage = "From: " . $service->users->getProfile('me')->getEmailAddress() . " <" . $service->users->getProfile('me')->getEmailAddress() . ">\r\n";
+            $strRawMessage .= "To: " . $_POST['email_address'] . " <" . $_POST['email_address'] . ">\r\n";
+            $strRawMessage .= 'Subject: =?utf-8?B?' . base64_encode($strSubject) . "?=\r\n";
+            $strRawMessage .= "MIME-Version: 1.0\r\n";
+            $strRawMessage .= "Content-Type: text/html; charset=utf-8\r\n";
+            $strRawMessage .= 'Content-Transfer-Encoding: quoted-printable' . "\r\n\r\n";
+            $strRawMessage .= $_POST['message'];
 
-        // The message needs to be encoded in Base64URL
-        $mime = rtrim(strtr(base64_encode($strRawMessage), '+/', '-_'), '=');
-        $msg = new Google_Service_Gmail_Message();
-        $msg->setRaw($mime);
+            // The message needs to be encoded in Base64URL
+            $mime = rtrim(strtr(base64_encode($strRawMessage), '+/', '-_'), '=');
+            $msg = new Google_Service_Gmail_Message();
+            $msg->setRaw($mime);
 
-        //The special value **me** can be used to indicate the authenticated user.
-        $service->users_messages->send("me", $msg);
+            //The special value **me** can be used to indicate the authenticated user.
+            $service->users_messages->send("me", $msg);
+        }
     }
-    echo '<a href="?enviar_email=true">Clieque aqui para enviar um email de teste</a>';
-    listMessages($service, 'me');
+    if (isset($_GET['listar_emails']) && $_GET['listar_emails']) {
+        listMessages($service, $user);
+    }
+    if (isset($_GET['list_emails_label']) && $_GET['list_emails_label']) {
+        listMessages($service, $user, array($_GET['list_emails_label']));
+    }
 } catch (Exception $exception) {
-    echo '<pre>';
-    var_dump($exception->getMessage());
-    echo '</pre>';
+    echo $exception->getMessage();
 }
 
